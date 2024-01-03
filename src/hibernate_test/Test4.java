1fa222eef244c3d553529e85f664ba00c3ac2aad
SergeyB19 <my.h.m@yandex.ru>
2024-01-03 21:11:34 +0300

    spring

---
+++ src/hibernate_test/Test4.java
@@ -0,0 +1,36 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test4 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+            /*меняем зарплату сотруднику с id №1*/
+//            Employee emp = session.get(Employee.class, 1);
+//            emp.setSalary(1500);
+            /*меняем зарплату сотрудникам по имени Александр на 1000*/
+            session.createQuery("update Employee set salary = 1000 " +
+                    "where name = 'Aleksandr'").executeUpdate();
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        } finally {
+            factory.close();
+        }
+    }
+}
+
+
+
