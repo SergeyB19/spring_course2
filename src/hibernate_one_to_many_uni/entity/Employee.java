0e9410ff9f7334b9400abc5f153225415060e353
SergeyB19 <my.h.m@yandex.ru>
2024-01-09 13:51:40 +0300

    spring(one to many) uni directional

---
+++ src/hibernate_one_to_many_uni/entity/Employee.java
@@ -0,0 +1,73 @@
+package hibernate_one_to_many_uni.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "employees1")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String firsName;
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "salary")
+    private int salary;
+
+
+    public Employee() {
+    }
+
+    public Employee(String firsName, String surname, int salary) {
+        this.firsName = firsName;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firsName='" + firsName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
